d166b970d05cd46208d222e3458c58d7e6df4c6e
Sonic Zhang <sonic.zhang@analog.com>
2009-06-30 10:02:21 +0000

    Fix bug[#5287] Fix typo error.
    git-svn-id: svn://localhost/svn/linux-kernel/branches/2009R1@6879 526b6c2d-f592-4532-a319-5dd88ccb003d

--- arch/blackfin/include/asm/cacheflush.h
+++ arch/blackfin/include/asm/cacheflush.h
@@ -95,7 +95,7 @@ do { memcpy(dst, src, len);						\
 #else
 # define invalidate_dcache_range(start,end)	do { } while (0)
 #endif
-#if defined(CONFIG_BFIN_L2_WB) && defined(CONFIG_BFIN_WB)
+#if defined(CONFIG_BFIN_L2_WB) || defined(CONFIG_BFIN_WB)
 # define flush_dcache_range(start,end)		blackfin_dcache_flush_range((start), (end))
 # define flush_dcache_page(page)			blackfin_dflush_page(page_address(page))
 #else

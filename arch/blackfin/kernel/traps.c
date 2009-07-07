7525f03d09865f947f108b2e1ab9c02dfb1fd059
Graf Yang <graf.yang@analog.com>
2009-07-07 09:33:38 +0000

    Fix bug [#5153], let ltp testcase mmap05 pass for MPU on branch
    git-svn-id: svn://localhost/svn/linux-kernel/branches/2009R1@6940 526b6c2d-f592-4532-a319-5dd88ccb003d

--- arch/blackfin/kernel/traps.c
+++ arch/blackfin/kernel/traps.c
@@ -395,7 +395,7 @@ asmlinkage void trap_c(struct pt_regs *fp)
 	/* 0x23 - Data CPLB protection violation, handled here */
 	case VEC_CPLB_VL:
 		info.si_code = ILL_CPLB_VI;
-		sig = SIGBUS;
+		sig = SIGSEGV;
 		verbose_printk(KERN_NOTICE EXC_0x23(KERN_NOTICE));
 		CHK_DEBUGGER_TRAP_MAYBE();
 		break;

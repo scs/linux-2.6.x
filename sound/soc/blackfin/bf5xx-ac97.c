b17f3b9ab82da2b412b50bbad4c3fb6b6ed86299
Cliff Cai <cliff.cai@analog.com>
2009-07-10 10:14:45 +0000

    bug[#5330]don't start playback/capture in resume,pcm driver will do it
    git-svn-id: svn://localhost/svn/linux-kernel/branches/2009R1@6961 526b6c2d-f592-4532-a319-5dd88ccb003d

--- sound/soc/blackfin/bf5xx-ac97.c
+++ sound/soc/blackfin/bf5xx-ac97.c
@@ -281,28 +281,24 @@ static int bf5xx_ac97_resume(struct platform_device *pdev,
 	if (!dai->active)
 		return 0;
 
-	ret = sport_set_multichannel(sport_handle, 16, 0x1F, 1);
+	ret = sport_set_multichannel(sport, 16, 0x1F, 1);
 	if (ret) {
 		pr_err("SPORT is busy!\n");
 		return -EBUSY;
 	}
 
-	ret = sport_config_rx(sport_handle, IRFS, 0xF, 0, (16*16-1));
+	ret = sport_config_rx(sport, IRFS, 0xF, 0, (16*16-1));
 	if (ret) {
 		pr_err("SPORT is busy!\n");
 		return -EBUSY;
 	}
 
-	ret = sport_config_tx(sport_handle, ITFS, 0xF, 0, (16*16-1));
+	ret = sport_config_tx(sport, ITFS, 0xF, 0, (16*16-1));
 	if (ret) {
 		pr_err("SPORT is busy!\n");
 		return -EBUSY;
 	}
 
-	if (dai->capture.active)
-		sport_rx_start(sport);
-	if (dai->playback.active)
-		sport_tx_start(sport);
 	return 0;
 }
 

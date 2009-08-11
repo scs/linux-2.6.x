9c36ac9b78a061186238245189adae35de5b1660
Sonic Zhang <sonic.zhang@analog.com>
2009-08-11 03:02:18 +0000

    Move MUSB anomaly error info into musb driver.
    git-svn-id: svn://localhost/svn/linux-kernel/branches/2009R1@7145 526b6c2d-f592-4532-a319-5dd88ccb003d

--- drivers/usb/musb/blackfin.h
+++ drivers/usb/musb/blackfin.h
@@ -42,6 +42,16 @@
  *             anomaly - 05000465.
  */
 
+/* The Mentor usb DMA engine on BF52x(silicon v0.0 and v0.1)seems
+ * not work stably in host mode. This may be caused by anomaly 05000380.
+ * After digging out the root cause anomaly, change its number  accordingly.
+ * So,either using silicon v0.2 or heighter or disabling DMA mode in musb.
+ */
+#if ANOMALY_05000380 && defined(CONFIG_BF52x) && \
+	defined(CONFIG_USB_MUSB_HDRC) && !defined(CONFIG_MUSB_PIO_ONLY)
+#error Please use PIO mode in MUSB driver on bf52x chip v0.0 and v0.1.
+#endif
+
 #undef DUMP_FIFO_DATA
 #ifdef DUMP_FIFO_DATA
 static void dump_fifo_data(u8 *buf, u16 len)
